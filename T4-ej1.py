60c217db09f2ed4c451d127ed3bba60bafcd04df
crisca-ull <73931424+crisca-ull@users.noreply.github.com>
2022-04-07 00:38:18 +0100

    Add files via upload

---
+++ T4-ej1.py
@@ -0,0 +1,8 @@
+# Escribe un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.
+
+Edad = int(input("¿Qué edad tienes? "))
+if Edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Aún eres menor de edad")
+    
